4e397151055bc1f88d7225d494ef82cd2ea54e1f
Brito79 <bshayamano2002@gmail.com>
2024-03-30 05:22:17 -0700

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,11 +8,7 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	(void)parent;
-	binary_tree_t *newnode = NULL;
-
-
-	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	binary_tree_t *newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	if (newnode == NULL)
 	{
